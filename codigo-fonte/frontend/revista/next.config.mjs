917f4c30658582f42d24f5ce9b4b07c9fddce02d
Leticia Mateus <145443338+Let788@users.noreply.github.com>
2025-11-24 02:12:17 +0000

    ts
    ts

--- codigo-fonte/frontend/revista/next.config.mjs
+++ codigo-fonte/frontend/revista/next.config.mjs
@@ -1,31 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // OBRIGATÓRIO para o Render (Static Site):
-  output: "export",
-
-  // IGNORAR ERROS NO BUILD (Permite o deploy mesmo com erros de lint/types)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  images: {
-    // OBRIGATÓRIO para Static Site:
-    unoptimized: true,
-
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      }
-    ],
-  },
+  // output: "export",  <-- REMOVA OU COMENTE ESSA LINHA
+  
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true }, // Opcional no Web Service, mas economiza CPU
 };
-
 export default nextConfig;
