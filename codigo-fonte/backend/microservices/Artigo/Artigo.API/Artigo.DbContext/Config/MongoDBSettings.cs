6b33b29b421ca1443a789cfca0fbba5f8e0f217f
Pedro de Oliveira Rosas <81116171+8BitsSpike@users.noreply.github.com>
2025-11-25 16:23:44 -0300

    CORS e seguranca

---
+++ codigo-fonte/backend/microservices/Artigo/Artigo.API/Artigo.DbContext/Config/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Artigo.DbContext.Config
+{
+    public class MongoDBSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+}
